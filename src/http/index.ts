d919f68b32468764aa900cb8b33c8afaab6d171d
mherMher <mher.melqonyan.mher123@gmail.com>
2026-06-26 14:58:29 +0400

    updateing front

--- src/http/index.ts
+++ src/http/index.ts
@@ -19,6 +19,10 @@ const onResponseError = (error: any) => {
   if ((status === 400 || status === 401) && typeof message === 'string' && message.startsWith('jwt')) {
     removeToken();
   }
+  if (status === 403) {
+    removeToken();
+    window.location.href = '/security/login';
+  }
   return Promise.reject(error);
 };
 
